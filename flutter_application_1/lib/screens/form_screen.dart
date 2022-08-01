fb2fc6a81afa26896bb0c3d9605379571a689098
Nuuteen <102475649+Nuuteen@users.noreply.github.com>
2022-08-01 20:02:57 +0700

    Lab5-5
    Form screen 2

--- flutter_application_1/lib/screens/form_screen.dart
+++ flutter_application_1/lib/screens/form_screen.dart
@@ -3,14 +3,37 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
 
 class FormScreen extends StatelessWidget {
-  const FormScreen({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text("แบบฟอร์มบันทึกข้อมูล"),
         ),
-        body: Container());
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Form(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextFormField(
+                  decoration: new InputDecoration(labelText: "ชื่อรายการ"),
+                  autofocus: true,
+                ),
+                TextFormField(
+                  decoration: new InputDecoration(labelText: "จำนวนเงิน"),
+                  keyboardType: TextInputType.number,
+                ),
+                FlatButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text("เพิ่มข้อมูล"),
+                  color: Colors.purple,
+                  textColor: Colors.white,
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
