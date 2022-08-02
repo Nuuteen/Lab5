67d0b42ecb862f4044a895ded291e4ddc50ee2a5
Nuuteen <102475649+Nuuteen@users.noreply.github.com>
2022-08-02 16:45:26 +0700

    Lab5-13
    Validate

--- flutter_application_1/lib/screens/form_screen.dart
+++ flutter_application_1/lib/screens/form_screen.dart
@@ -1,8 +1,7 @@
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
 
 class FormScreen extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,28 +11,50 @@ class FormScreen extends StatelessWidget {
         body: Padding(
           padding: const EdgeInsets.all(10.0),
           child: Form(
+            key: _formKey,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 TextFormField(
                   decoration: new InputDecoration(labelText: "ชื่อรายการ"),
                   autofocus: true,
+                  validator: (str) {
+                    var c_str = str!.isEmpty;
+                    if (str == null) {
+                      return "กรุณาป้อนชื่อรายการ";
+                    }
+                    return null;
+                  },
                 ),
                 TextFormField(
                   decoration: new InputDecoration(labelText: "จำนวนเงิน"),
                   keyboardType: TextInputType.number,
+                  validator: (str) {
+                    var c_str = str!.isEmpty;
+                    if (str == null) {
+                      return "กรุณาป้อนจำนวนเงิน";
+                    }
+                    if (double.parse(str) <= 0) {
+                      return "ระบุ มากกว่า 0";
+                    }
+                    return null;
+                  },
                 ),
                 FlatButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
                   child: Text("เพิ่มข้อมูล"),
                   color: Colors.purple,
                   textColor: Colors.white,
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      Navigator.pop(context);
+                    }
+                  },
                 )
               ],
             ),
           ),
         ));
   }
+
+  newMethod() => _formKey.currentState!.validate();
 }
